7e43743d7537a4469b24a9bf9a7dff8ae97450c8
Juul03 <118166691+Juul03@users.noreply.github.com>
2024-01-31 17:30:05 +0100

    added header component

---
+++ src/components/header.svelte
@@ -0,0 +1,19 @@
+<!-- TODO: link the a elements to the right element on the page voor #mijn werk en # contact -->
+<header>
+	<nav>
+		<ul>
+			<li>
+				<a href="/"><img src="/favicon.svg" alt="Home" /></a>
+			</li>
+            <li>
+                <a href="/">Mijn werk</a>
+            </li>
+            <li>
+                <a href="/over-mij">Over mij</a>
+            </li>
+            <li>
+                <a href="/">Contact</a>
+            </li>
+		</ul>
+	</nav>
+</header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Header from '../components/header.svelte';
+</script>
+
+<Header/>
+
+<header>
+    <h1>Hey, ik ben Julia</h1>
+    <h2>(Web)designer - frontend developer</h2>
+    <p>
+        Derdejaars Communicatie en Multimedia Design (CMD) student aan de HvA, die houdt van van de kracht
+        en schoonheid van UX/UI design in combinatie met de uitdaging van het developen.
+    </p>
+    <a href="/over-mij">Over mij</a>
+    <a href="/">Mijn werk</a>
+
+    <img src="/img/people/me.svg" alt="Dit ben ik! Julia">
+</header>
