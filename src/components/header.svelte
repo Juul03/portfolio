06a77e390f6d588224b8378a02dda34e5ff35b86
Juul03 <118166691+Juul03@users.noreply.github.com>
2024-02-01 11:35:52 +0100

    header_styling_added

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -5,31 +5,44 @@
 			<li>
 				<a href="/"><img src="/favicon.svg" alt="Home" /></a>
 			</li>
-            <li>
-                <a href="/">Mijn werk</a>
-            </li>
-            <li>
-                <a href="/over-mij">Over mij</a>
-            </li>
-            <li>
-                <a href="/">Contact</a>
-            </li>
+			<li>
+				<a href="/">Mijn werk</a>
+			</li>
+			<li>
+				<a href="/over-mij">Over mij</a>
+			</li>
+			<li>
+				<a href="/">Contact</a>
+			</li>
 		</ul>
 	</nav>
 </header>
 
 <style lang="scss">
-    header {
-      width: 100%;
-      background: var(--primary-color);
-  
-      nav {
-        width: 100%;
-  
-        ul {
-          display: flex;
-          flex-direction: row;
-        }
-      }
-    }
+	header {
+		width: 100%;
+		background: var(--secundary-color);
+
+		nav {
+			width: 100%;
+
+			ul {
+				display: flex;
+				flex-direction: row;
+				align-items: center;
+
+				li:first-child {
+					flex-grow: 9;
+				}
+
+				li:not(:first-child) {
+					flex-grow: 1;
+				}
+
+                li > a {
+                    color:var(--primary-color);
+                }
+			}
+		}
+	}
 </style>
