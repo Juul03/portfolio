74870ee6a783fccb93c150bbb859e329f06e716f
Juul03 <118166691+Juul03@users.noreply.github.com>
2024-02-01 09:46:46 +0100

    show_fetched_projects_title_forEach

--- src/components/projects.svelte
+++ src/components/projects.svelte
@@ -1,30 +1,37 @@
 <script>
-    let myProjectsData;
+	import { onMount } from 'svelte';
 
-    let fetchData = async () => {
-        try {
-            const response = await fetch('/data/my-work-details.json');
+	let myProjectsData;
 
-            if (response.ok) {
-            console.log("We got the projects!");
-            const data = await response.json();
-            myProjectsData = data; // Store the JSON data in myProjectsData
-            console.log("myProjectsData:", myProjectsData);
-            } else {
-            console.error("Failed to fetch projects:", response.status);
-            }
-        } catch (error) {
-            console.error("Error fetching projects:", error);
-        }
-    };
+	let fetchData = async () => {
+		try {
+			const response = await fetch('/data/my-work-details.json');
 
-    fetchData();
+			if (response.ok) {
+				const data = await response.json();
+				myProjectsData = data;
+			} else {
+				console.error('Failed to fetch projects:', response.status);
+			}
+		} catch (error) {
+			console.error('Error fetching projects:', error);
+		}
+	};
+
+	fetchData();
 </script>
 
 <section>
-    <h2>My Work</h2>
+	<h2>My Work</h2>
 
-    <section>
-        <!-- For each -->
-    </section>
+	<section>
+		<!-- For each -->
+		{#if myProjectsData}
+			{#each myProjectsData as project}
+				{project.project_name}
+			{/each}
+		{:else}
+			<p>Sorry, er ging iets mis met het fetchen van mijn projecten. Laat het mij weten!</p>
+		{/if}
+	</section>
 </section>
