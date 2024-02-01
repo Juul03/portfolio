a97356f9457df44902dae5e7f72d8ad5fb930da4
Juul03 <118166691+Juul03@users.noreply.github.com>
2024-02-01 12:03:07 +0100

    header_styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,15 +8,17 @@
 <Header/>
 
 <header>
-    <h1>Hey, ik ben Julia</h1>
-    <h2>(Web)designer - frontend developer</h2>
-    <p>
-        Derdejaars Communicatie en Multimedia Design (CMD) student aan de HvA, die houdt van van de kracht
-        en schoonheid van UX/UI design in combinatie met de uitdaging van het developen.
-    </p>
-    <a href="/over-mij">Over mij</a>
-    <a href="/">Mijn werk</a>
-
+    <div>
+        <h1>Hey, ik ben Julia</h1>
+        <h2>(Web)designer - frontend developer</h2>
+        <p>
+            Derdejaars Communicatie en Multimedia Design (CMD) student aan de HvA, die houdt van van de kracht
+            en schoonheid van UX/UI design in combinatie met de uitdaging van het developen.
+        </p>
+        <a class="secundary-button" href="/over-mij">Over mij</a>
+        <a class="primary-button" href="/">Mijn werk</a>
+    </div>
+    
     <img src="/img/people/me.svg" alt="Dit ben ik! Julia">
 </header>
 
@@ -29,3 +31,31 @@
 </main>
 
 <Footer/>
+
+<style lang="scss">
+    header {
+        background-color: rgba(var(--primary-color-rgb), var(--background-alpha));
+        padding:var(--standard-padding-section) 0 var(--standard-padding-section);
+
+        display:flex;
+        justify-content:space-around;
+
+        div {
+            max-width:50%;
+
+            p {
+                margin-bottom:3rem;
+            }
+
+            a {
+
+            }
+        }
+
+        img {
+            max-width:30%;
+            order:-1;
+        }
+
+    }
+</style>
