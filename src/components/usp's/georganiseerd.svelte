b088140934592076048d435ec2e51c76397bbe0c
Juul03 <118166691+Juul03@users.noreply.github.com>
2024-02-01 09:22:02 +0100

    added_html_usps

---
+++ src/components/usp's/georganiseerd.svelte
@@ -0,0 +1,4 @@
+<section>
+    <h3>Georganiseerd</h3>
+    <p>Laat mij maar een plan maken met alles wat er moet gebeuren, zo kunnen we binnen een bepaalde tijd zoveel mogelijk uit elkaar en uit het project halen!</p>
+</section>
---
+++ src/components/usp's/visual-thinker.svelte
@@ -0,0 +1,4 @@
+<section>
+    <h3>Visual Thinker</h3>
+    <p>Mijn ideeën en gedachten visualiseer ik door bijvoorbeeld gebruik te maken van iconen of kleine ondersteunende tekeningen</p>
+</section>
