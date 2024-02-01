72ec112d7165042c217bf60e29afff7cd4b96e7c
Juul03 <118166691+Juul03@users.noreply.github.com>
2024-02-01 10:05:52 +0100

    added_projects_for_each

--- src/components/projects.svelte
+++ src/components/projects.svelte
@@ -22,16 +22,51 @@
 </script>
 
 <section>
-	<h2>My Work</h2>
+	<h2>Mijn werk</h2>
 
 	<section>
 		<!-- For each -->
 		{#if myProjectsData}
 			{#each myProjectsData as project}
-				{project.project_name}
+				<section>
+					<h3>{project.project_name}</h3>
+					<div>
+						<h4>{project.project_tags}</h4>
+						<img src="/img/icons/group-project.svg" alt="group project icon" />
+					</div>
+
+					<p>{project.project_description_short}</p>
+
+					<ul>
+						<li>Skills: {project.used_skills}</li>
+						<li>
+							Opdrachtgever: {project.client_name}
+							{#if project.client_job_function !== ''}
+								{project.client_job_function}
+							{/if}
+
+							{#if project.non_fiction_client == 'Y'}
+								(non-fictief)
+							{:else}
+								(fictief)
+							{/if}
+						</li>
+					</ul>
+
+                    <!-- TODO: add link in json file and import here -->
+                    <a href="/">Lees meer</a>
+
+                    <img src={project.project_image_src} alt="">
+				</section>
 			{/each}
 		{:else}
 			<p>Sorry, er ging iets mis met het fetchen van mijn projecten. Laat het mij weten!</p>
 		{/if}
 	</section>
 </section>
+
+<style>
+    img {
+        max-width:40%;
+    }
+</style>
