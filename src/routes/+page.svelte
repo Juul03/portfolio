7eae9794144df46bc106c7aa20eac41030a90273
Juul03 <118166691+Juul03@users.noreply.github.com>
2024-02-02 11:40:10 +0100

    header_styling_buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,61 +1,74 @@
 <script>
-    import Header from '../components/header.svelte';
-    import Usps from '../components/usps/usps.svelte';
-    import Projects from '../components/projects.svelte';
-    import Footer from '../components/footer.svelte';
+	import Header from '../components/header.svelte';
+	import Usps from '../components/usps/usps.svelte';
+	import Projects from '../components/projects.svelte';
+	import Footer from '../components/footer.svelte';
 </script>
 
-<Header/>
+<Header />
 
 <header>
-    <div>
-        <h1>Hey, ik ben Julia</h1>
-        <h2>(Web)designer - frontend developer</h2>
-        <p>
-            Derdejaars Communicatie en Multimedia Design (CMD) student aan de HvA, die houdt van van de kracht
-            en schoonheid van UX/UI design in combinatie met de uitdaging van het developen.
-        </p>
-        <a class="secundary-button" href="/over-mij">Over mij</a>
-        <a class="primary-button" href="/">Mijn werk</a>
-    </div>
-    
-    <img src="/img/people/me.svg" alt="Dit ben ik! Julia">
+	<div>
+		<h1>Hey, ik ben Julia</h1>
+		<h2>(Web)designer - frontend developer</h2>
+		<p>
+			Derdejaars Communicatie en Multimedia Design (CMD) student aan de HvA, die houdt van van de
+			kracht en schoonheid van UX/UI design in combinatie met de uitdaging van het developen.
+		</p>
+		<div>
+			<a class="secundary-button" href="/over-mij">Over mij</a>
+			<a class="primary-button" href="/">Mijn werk</a>
+		</div>
+	</div>
+
+	<img src="/img/people/me.svg" alt="Dit ben ik! Julia" />
 </header>
 
 <main>
+	<Usps />
 
-    <Usps/>
-
-    <Projects/>
-
+	<Projects />
 </main>
 
-<Footer/>
+<Footer />
 
 <style lang="scss">
-    header {
-        background-color: rgba(var(--primary-color-rgb), var(--background-alpha));
-        padding:var(--standard-padding-section) 0 var(--standard-padding-section);
+	header {
+		background-color: rgba(var(--primary-color-rgb), var(--background-alpha));
+		padding: var(--standard-padding-section) 0 var(--standard-padding-section);
 
-        display:flex;
-        justify-content:space-around;
+		display: flex;
+		justify-content: space-around;
 
-        div {
-            max-width:50%;
+		div {
+			max-width: 50%;
+			display: flex;
+			flex-direction: column;
 
-            p {
-                margin-bottom:3rem;
+			h2 {
+				order: -1;
+			}
+
+			p {
+				margin-bottom: 3rem;
+			}
+
+			div {
+                display:flex;
+                flex-direction:row;
+                width:100%;
+
+                gap:1.4rem;
+
+                a {
+
+                }
             }
+		}
 
-            a {
-
-            }
-        }
-
-        img {
-            max-width:30%;
-            order:-1;
-        }
-
-    }
+		img {
+			max-width: 30%;
+			order: -1;
+		}
+	}
 </style>
