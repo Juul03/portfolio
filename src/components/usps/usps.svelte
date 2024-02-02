d5c74caada99e15e4eed2dd15bd4cc06ffeecb97
Juul03 <118166691+Juul03@users.noreply.github.com>
2024-02-02 11:52:28 +0100

    styling_usp_section

--- src/components/usps/usps.svelte
+++ src/components/usps/usps.svelte
@@ -1,18 +1,35 @@
 <script>
-    import VisualThinker from './visual-thinker.svelte';
-    import ZelfstandigeTeamplayer from './zelfstandige-teamplayer.svelte';
-    import NieuwsgierigLeergierig from './nieuwsgierig-leergierig.svelte';
-    import Verantwoordelijk from './verantwoordelijk.svelte';
-    import Georganiseerd from './georganiseerd.svelte';
+	import VisualThinker from './visual-thinker.svelte';
+	import ZelfstandigeTeamplayer from './zelfstandige-teamplayer.svelte';
+	import NieuwsgierigLeergierig from './nieuwsgierig-leergierig.svelte';
+	import Verantwoordelijk from './verantwoordelijk.svelte';
+	import Georganiseerd from './georganiseerd.svelte';
 </script>
 
 <section>
-    <VisualThinker/>
-    <ZelfstandigeTeamplayer/>
-    <NieuwsgierigLeergierig/>
-    <Verantwoordelijk/>
-    <Georganiseerd/>
+	<VisualThinker />
+	<ZelfstandigeTeamplayer />
+	<NieuwsgierigLeergierig />
+	<Verantwoordelijk />
+	<Georganiseerd />
 
-    <img src="/img/icons/usp-icons.svg" alt="usp icons">
-    
+	<img src="/img/icons/usp-icons.svg" alt="usp icons" />
 </section>
+
+<style lang="scss">
+	section {
+		padding: var(--standard-padding-section);
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+		grid-template-rows: repeat(2, 1fr);
+		gap: 4.3rem;
+
+		img {
+			grid-column: 2; 
+            grid-row:2;
+			justify-self: center;
+			align-self: center;
+            width:100%;
+		}
+	}
+</style>
