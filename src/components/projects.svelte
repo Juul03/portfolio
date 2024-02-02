90e10a0e423b86648315bc2fba1f165d799641a0
Juul03 <118166691+Juul03@users.noreply.github.com>
2024-02-02 13:49:12 +0100

    projects_sections_styled

--- src/components/projects.svelte
+++ src/components/projects.svelte
@@ -35,7 +35,9 @@
 		}
 	};
 
-	fetchData();
+	onMount(async () => {
+		fetchData();
+	});
 </script>
 
 <section id="projects">
@@ -46,32 +48,34 @@
 		{#if myProjectsData}
 			{#each myProjectsData as project}
 				<section>
-					<h3>{project.project_name}</h3>
 					<div>
-						<h4>{project.project_tags}</h4>
-						<img src="/img/icons/group-project.svg" alt="group project icon" />
+						<h3>{project.project_name}</h3>
+						<div>
+							<h4>{project.project_tags}</h4>
+							<img src="/img/icons/group-project.svg" alt="group project icon" />
+						</div>
+
+						<p>{project.project_description_short}</p>
+
+						<ul>
+							<li>Skills: {project.used_skills}</li>
+							<li>
+								Opdrachtgever: {project.client_name}
+								{#if project.client_job_function !== ''}
+									{project.client_job_function}
+								{/if}
+
+								{#if project.non_fiction_client == 'Y'}
+									(non-fictief)
+								{:else}
+									(fictief)
+								{/if}
+							</li>
+						</ul>
+
+						<!-- TODO: add link in json file and import here -->
+						<a class="primary-button" href="/">Lees meer</a>
 					</div>
-
-					<p>{project.project_description_short}</p>
-
-					<ul>
-						<li>Skills: {project.used_skills}</li>
-						<li>
-							Opdrachtgever: {project.client_name}
-							{#if project.client_job_function !== ''}
-								{project.client_job_function}
-							{/if}
-
-							{#if project.non_fiction_client == 'Y'}
-								(non-fictief)
-							{:else}
-								(fictief)
-							{/if}
-						</li>
-					</ul>
-
-					<!-- TODO: add link in json file and import here -->
-					<a href="/">Lees meer</a>
 
 					<img src={project.project_image_src} alt="" />
 				</section>
@@ -86,20 +90,72 @@
 	section {
 		h2 {
 			padding-left: var(--standard-padding-section);
+			font-size: var(--heading-1-font-size);
 		}
 
 		> section > section {
 			padding: var(--standard-padding-section);
+            display: flex;
+		    justify-content: space-between;
+
+			> div {
+				display: flex;
+				flex-direction: column;
+                width: 50%;
+
+				h3 {
+					font-size: var(--heading-2-font-size);
+				}
+
+				> div:nth-of-type(1) {
+					order: -1;
+
+					display: flex;
+					align-items: center; /* Align items vertically */
+					gap: 1rem; /* Adjust the gap between h4 and img as needed */
+
+					img {
+						height: 2rem;
+						width: auto;
+						order: -1;
+					}
+
+					h4 {
+						font-size: var(--heading-3-font-size);
+						margin: 0; /* Remove default margin */
+					}
+				}
+
+				ul li {
+					color: var(--primary-color);
+				}
+
+                a {
+                    width:max-content;
+                }
+			}
+
 		}
 		> section > section:nth-of-type(odd) {
 			background-color: rgba(var(--primary-color-rgb), var(--background-alpha));
+
+            > img {
+				width: 40%;
+                height:25rem;
+                object-fit: cover;
+			}
 		}
 
 		> section > section:nth-of-type(even) {
 			background: var(--secundary-color);
+
+            > img {
+                width: 40%;
+                height:25rem;
+                background:red;
+                object-fit: cover;
+                order:-1;
+            }
 		}
-	}
-	img {
-		max-width: 40%;
 	}
 </style>
