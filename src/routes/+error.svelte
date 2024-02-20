fc9d12bbeb02c06b03847157b1d55df790fb1ed4
Juul03 <118166691+Juul03@users.noreply.github.com>
2024-02-20 15:45:49 +0100

    error_page_added

---
+++ src/routes/+error.svelte
@@ -0,0 +1,31 @@
+<main>
+    <section>
+        <h1>404</h1>
+        <h2>Oops! Deze pagina bestaat niet</h2>
+    </section>
+
+    <a class="primary-button" href="/">Ik breng je terug naar home</a>
+</main>
+
+<style>
+    * {
+        text-align:center;
+    }
+
+    main {
+        display:flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items:center;
+
+        height:100vh;
+    }
+
+    section {
+        width:50%;
+        display:flex;
+        align-items:center;
+        flex-direction:column;
+
+    }
+</style>
