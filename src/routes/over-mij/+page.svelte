c68c2af69fb6382660d1b0837a8b939670ebf2ad
Juul03 <118166691+Juul03@users.noreply.github.com>
2024-04-20 14:17:47 +0200

    html_skeleton_CV_page

--- src/routes/over-mij/+page.svelte
+++ src/routes/over-mij/+page.svelte
@@ -1,1 +1,47 @@
-<h1>Over mij</h1>
+<section>
+    <h1>Over mij</h1>
+    <h2>(Web)designer - frontend developer</h2>
+    <p>
+        Hey, ik ben Julia, 21 jarige derdejaars Communicatie en Multimedia Design (CMD) student aan de HvA, die houdt van van de
+        kracht en schoonheid van UX/UI design in combinatie met de uitdaging van het developen. Daarom ben ik ook opzoek naar een
+        leuke stage die een combinatie van beide biedt!
+    </p>
+    <p>
+        iets over soft en hardskills, wat anderen over mij zeggen
+    </p>
+    <p>leerdoelen en aflsuitng</p>
+</section>
+
+<section>
+    <h2>Skills</h2>
+    <section>
+        <h3>Hardskills</h3>
+        <section>
+            <h4>Tech</h4>
+            <ul>
+                <li></li>
+            </ul>
+        </section>
+        <section>
+            <h4>Design</h4>
+            <ul>
+                <li></li>
+                <li>UX-werk & onderzoeksmethoden</li>
+            </ul>
+        </section>
+    </section>
+    <section>
+        <h3>Softskills</h3>
+        <ul>
+            <li></li>
+        </ul>
+    </section>
+
+    <section>
+        <h3>Overig</h3>
+        <ul>
+            <li>Nederlands (moedertaal)</li>
+            <li>Engels (afgerond niveau C1)</li>
+        </ul>
+    </section>
+</section>
